fc80dae2ddca6cfd52628cb7a68a788b82c38bbe
Caroline Courtney <caroline@carolinecourtney.com>
2020-03-08 12:44:13 -0400

    Connect Notes and Tasks fragments to the navigation

--- app/src/main/java/navigation/NavigationActivity.kt
+++ app/src/main/java/navigation/NavigationActivity.kt
@@ -2,27 +2,39 @@ package navigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
+import androidx.fragment.app.Fragment
 import com.carolinecourtney.notes.R
+import notes.NotesListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_navigation.*
+import tasks.TasksListFragment
 
 class NavigationActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_notes -> {
+                replaceFragment(NotesListFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_tasks -> {
+                replaceFragment(TasksListFragment.newInstance())
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
-        val navView = findViewById<BottomNavigationView>(R.id.nav_view)
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_tasks,
-            R.id.navigation_notes
-        )
-            .build()
-        val navController =
-            Navigation.findNavController(this,
-                R.id.nav_host_fragment
-            )
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
-        NavigationUI.setupWithNavController(navView, navController)
+        replaceFragment(TasksListFragment.newInstance())
+        navigation_view.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, fragment)
+            .commit()
     }
 }
