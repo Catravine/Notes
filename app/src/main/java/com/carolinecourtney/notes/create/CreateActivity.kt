5a15800b2897f49df6aa392571680cbf79618861
Caroline Courtney <caroline@carolinecourtney.com>
2020-03-28 13:07:23 -0400

    start of creation fragments

--- app/src/main/java/com/carolinecourtney/notes/create/CreateActivity.kt
+++ app/src/main/java/com/carolinecourtney/notes/create/CreateActivity.kt
@@ -1,22 +1,50 @@
 package com.carolinecourtney.notes.create
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.carolinecourtney.notes.R
 import com.carolinecourtney.notes.navigation.NavigationActivity
-import kotlinx.android.synthetic.main.activity_create.*
 
-class CreateActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity(), CreateNoteFragment.OnFragmentInteractionListener, CreateTaskFragment.OnFragmentInteractionListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        supportActionBar?.title = ""
+
         intent.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY).run {
-            textView.text = if (this == NavigationActivity.FRAGMENT_VALUE_NOTE) {
-                "This is a Note!"
-            } else if (this == NavigationActivity.FRAGMENT_VALUE_TASK) {
-                "This is a Task!"
-            } else "this is nothing."
+            if (this == NavigationActivity.FRAGMENT_VALUE_TASK) {
+                createFragment(CreateTaskFragment.newInstance())
+            } else if (this == NavigationActivity.FRAGMENT_VALUE_NOTE) {
+                createFragment(CreateNoteFragment.newInstance())
+            }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId) {
+            R.id.save_item -> Toast.makeText(this, "Save Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_holder, fragment)
+            .commit()
+    }
+
+    override fun onFragmentInteraction() {
+
     }
 }
---
+++ app/src/main/java/com/carolinecourtney/notes/create/CreateTaskFragment.kt
@@ -0,0 +1,30 @@
+package com.carolinecourtney.notes.create
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.carolinecourtney.notes.R
+
+class CreateTaskFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_create_task, container, false)
+    }
+
+    interface OnFragmentInteractionListener {
+        fun onFragmentInteraction()
+    }
+
+    companion object {
+        fun newInstance() = CreateTaskFragment()
+    }
+}
